3604adb82a4d77c35399bf7bb40514128b97de8b
theianjones <jones58ian@gmail.com>
2022-03-24 09:53:00 -0400

    format

--- test/index.js
+++ test/index.js
@@ -5,43 +5,43 @@ import arrayFixture from './arrayFixture.json';
 
 // example of an import'd plugin
 const CustomPlugin = (config) => ({
-    ...config,
-    name: 'custom-plugin'
+		...config,
+		name: 'custom-plugin'
 });
 
 var json = (
-    <webpack target="web" watch>
-        <entry path="src/index.js" />
-        <resolve>
-                <alias from="react-dom" to="preact-compat" />
-        </resolve>
-        <plugins>
-                <uglify-js opts={{
-                        compression: true,
-                        mangle: false,
-                }} />
-                <CustomPlugin foo="bar" />
-        </plugins>
-    </webpack>
+		<webpack target="web" watch>
+				<entry path="src/index.js" />
+				<resolve>
+					<alias from="react-dom" to="preact-compat" />
+				</resolve>
+				<plugins>
+							<uglify-js opts={{
+												compression: true,
+												 mangle: false,
+							}} />
+							<CustomPlugin foo="bar" />
+				</plugins>
+		</webpack>
 );
 
 assert.deepEqual(json, fixture);
 
 var arrayJson = (
-    <webpack target="web" watch>
-        <entry path="src/index.js" />
-        <resolve>
-            <alias from="react" to="preact-compat" />
-            <alias from="react-dom" to="preact-compat" />
-        </resolve>
-        <plugins>
-            <uglify-js opts={{
-                    compression: true,
-                    mangle: false,
-                }} />
-            <CustomPlugin foo="bar" />
-        </plugins>
-    </webpack>
+		<webpack target="web" watch>
+				<entry path="src/index.js" />
+				<resolve>
+						<alias from="react" to="preact-compat" />
+						<alias from="react-dom" to="preact-compat" />
+				</resolve>
+				<plugins>
+						<uglify-js opts={{
+										compression: true,
+										mangle: false,
+								}} />
+						<CustomPlugin foo="bar" />
+				</plugins>
+		</webpack>
 );
 
 assert.deepEqual(arrayJson, arrayFixture);
